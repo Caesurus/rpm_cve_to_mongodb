56636dd72ee46e30222bd8e1ab1cbe224615d64e
Caesurus <caesurus@gmail.com>
2019-11-09 21:39:27 -0500

    Initial Commit

---
+++ rpm_cve_to_mongo.py
@@ -0,0 +1,98 @@
+import argparse
+import os
+
+import pymongo
+import requests
+import xmltodict as xmltodict
+from pymongo import UpdateOne
+
+
+class RPMCVE2Mongo(object):
+    def __init__(self, server="mongodb://172.16.202.44:27017", dbuser=None, dbpasswd=None,
+                 dbauthsource='admin', rpm_to_cve_url=None, dbname="RPMINFO", dbcollection="rpm2cve"):
+
+        self.raw_rpm_cve_data = None
+        if rpm_to_cve_url:
+            self.rpm_to_cve_url = rpm_to_cve_url
+        else:
+            self.rpm_to_cve_url = 'https://www.redhat.com/security/data/metrics/rpm-to-cve.xml'
+
+        self.server = server
+
+        if not dbuser:
+            raise RuntimeError("dbuser needs to be set")
+
+        if not dbpasswd:
+            raise RuntimeError("dbpasswd needs to be set")
+
+        self.dbuser = dbuser
+        self.dbpasswd = dbpasswd
+        self.dbauthsource = dbauthsource
+        self.dbcollection = dbcollection
+
+        self.client = pymongo.MongoClient(server, username=self.dbuser,
+                                          password=self.dbpasswd,
+                                          authSource=self.dbauthsource,
+                                          authMechanism='SCRAM-SHA-1',
+                                          serverSelectionTimeoutMS=5)
+
+        self.db = self.client[dbname]
+
+    def create_indexes(self):
+        self.db[self.dbcollection].create_index([("@rpm", pymongo.ASCENDING)], background=True)
+
+    def download_list(self):
+        r = requests.get(self.rpm_to_cve_url)
+        if 200 == r.status_code:
+            self.raw_rpm_cve_data = r.content
+
+    def upsert_to_mongo(self):
+        collection = self.db[self.dbcollection]
+
+        operations = []
+        rpms = xmltodict.parse(self.raw_rpm_cve_data)
+        for rpm in rpms['rpms']['rpm']:
+            tmp = UpdateOne({"@rpm": rpm['@rpm']}, {"$set": rpm}, upsert=True)
+            operations.append(tmp)
+
+        if len(operations):
+            print("Inserting %d docs" % len(operations))
+            result = collection.bulk_write(operations, ordered=False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Pull rpm to cve list from RedHat and upsert into Mongo')
+    parser.add_argument("-v", "--verbose", help="modify output verbosity", action="store_true")
+    parser.add_argument("-u", "--user", help="specify user for mongodb", action="store")
+    parser.add_argument("-p", "--passwd", help="specify password for mongodb", action="store")
+    parser.add_argument("-s", "--server", help="specify server connection string: EG mongodb://serverip:port", action="store")
+
+    args = parser.parse_args()
+
+    if args.user:
+        dbuser = args.user
+    elif "DBUSER" in os.environ:
+        dbuser = os.environ["DBUSER"]
+    else:
+        raise RuntimeError("Please specify user, either -u <username>, or export DBUSER=username")
+
+    if args.passwd:
+        dbpasswd = args.passwd
+    elif "DBPASSWD" in os.environ:
+        dbpasswd = os.environ["DBPASSWD"]
+    else:
+        raise RuntimeError("Please specify password, either -p <passwd>, or export DBPASSWD=password")
+
+    if args.server:
+        dbserver = args.server
+    elif "DBSERVER" in os.environ:
+        dbserver = os.environ["DBSERVER"]
+    else:
+        raise RuntimeError("Please specify server, either -s <server>, or export DBSERVER=\"mongodb://serverip:27017\"")
+
+    rpm_cve_2_mongo = RPMCVE2Mongo(server=dbserver, dbuser=dbuser, dbpasswd=dbpasswd)
+
+    rpm_cve_2_mongo.create_indexes()
+    rpm_cve_2_mongo.download_list()
+    rpm_cve_2_mongo.upsert_to_mongo()
+    print("DONE")
---
+++ rpm_cve_to_mongo_test.py
@@ -0,0 +1,73 @@
+import unittest
+import requests_mock
+import mongomock as mongomock
+
+from rpm_cve_to_mongo import RPMCVE2Mongo
+
+test_xml = """<rpms>
+  <rpm rpm="htdig-2:3.1.6-7.el3">
+    <erratum released="2007-06-07">RHBA-2007:0026</erratum>
+    <cve>CVE-2000-1191</cve>
+  </rpm>
+  <rpm rpm="kernel-doc-0:2.6.9-55.EL">
+    <erratum released="2007-04-28">RHBA-2007:0304</erratum>
+    <cve>CVE-2005-2873</cve>
+    <cve>CVE-2005-3257</cve>
+    <cve>CVE-2006-0557</cve>
+    <cve>CVE-2006-1863</cve>
+    <cve>CVE-2007-1592</cve>
+    <cve>CVE-2007-3379</cve>
+  </rpm>
+  <rpm rpm="kernel-smp-0:2.6.9-55.EL">
+    <erratum released="2007-04-28">RHBA-2007:0304</erratum>
+    <cve>CVE-2005-2873</cve>
+    <cve>CVE-2005-3257</cve>
+    <cve>CVE-2006-0557</cve>
+    <cve>CVE-2006-1863</cve>
+    <cve>CVE-2007-1592</cve>
+    <cve>CVE-2007-3379</cve>
+  </rpm>
+</rpms>
+"""
+
+
+class RPMCVEToMongoTestCase(unittest.TestCase):
+
+    @mongomock.patch(servers=(('mongodb://localhost', "27017")))
+    def test_instance(self):
+        rpm_cve_2_mongo = RPMCVE2Mongo(server="mongodb://user:passwd@localhost", dbuser="AAA", dbpasswd="pass")
+        self.assertIsInstance(rpm_cve_2_mongo, RPMCVE2Mongo)
+
+    def test_get_list(self):
+        rpm_cve_2_mongo = RPMCVE2Mongo(server="mongodb://user:passwd@localhost", dbuser="AAA", dbpasswd="pass")
+
+        with requests_mock.mock() as m:
+            expected_data = "datacontents"
+            m.get("https://www.redhat.com/security/data/metrics/rpm-to-cve.xml", text=expected_data)
+
+            rpm_cve_2_mongo.download_list()
+            self.assertEqual(b'datacontents', rpm_cve_2_mongo.raw_rpm_cve_data)
+
+    @mongomock.patch(servers=(('mongodb://user:passwd@localhost', "27017")))
+    def test_upsert_to_mongo(self):
+        rpm_cve_2_mongo = RPMCVE2Mongo(server="mongodb://user:passwd@localhost", dbuser="AAA", dbpasswd="pass")
+
+        with requests_mock.mock() as m:
+            m.get("https://www.redhat.com/security/data/metrics/rpm-to-cve.xml", text=test_xml)
+            rpm_cve_2_mongo.download_list()
+            rpm_cve_2_mongo.upsert_to_mongo()
+        collection = rpm_cve_2_mongo.db[rpm_cve_2_mongo.dbcollection]
+
+        self.assertEqual(3, collection.count())
+        result = collection.find({"@rpm":"kernel-doc-0:2.6.9-55.EL"})
+        cve_expected = [ 'CVE-2005-2873',
+                         'CVE-2005-3257',
+                         'CVE-2006-0557',
+                         'CVE-2006-1863',
+                         'CVE-2007-1592',
+                         'CVE-2007-3379']
+        self.assertEqual(cve_expected, result[0]['cve'])
+
+
+if __name__ == '__main__':
+    unittest.main()
